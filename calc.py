9ae74a201f15ed03532ae246918c34cfb8597a59
Emre Uyan <76150144+EmreUyan@users.noreply.github.com>
2021-07-31 13:18:56 +0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,37 @@
+import math
+from math import sqrt
+
+print(">>>")
+s1 = int(input("1st Number: "))
+s2 = int(input("2nd Number: "))
+s3 = input("Operator: ")
+
+if s3 == "+" :
+    print(">>>")
+    print(s1 + s2)
+
+elif s3 == "-" :
+    print(">>>")
+    print(s1 - s2)
+
+elif s3 == "*" :
+    print(">>>")
+    print(s1 * s2)
+
+elif s3 == "/" :
+    print(">>>")
+    print(s1 / s2)
+
+elif s3 == "k" :
+    print(">>>")
+    print(s1 * s1 + s2 * s2)
+
+elif s3 == "kk" :
+    print(">>>")
+    print(sqrt(s1))
+
+else:
+    print(">>>")
+    print("Error. (Please type an operator)")
+
+    
